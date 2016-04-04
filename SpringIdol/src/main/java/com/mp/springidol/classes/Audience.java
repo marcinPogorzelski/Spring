22b28a9de879c0adecbf016d36fab8b1b8920d8a
Marcin Pogorzelski <pogorzelski.marcin@gmail.com>
2016-04-04 22:25:17 +0200

    Dodane aspekty

---
+++ SpringIdol/src/main/java/com/mp/springidol/classes/Audience.java
@@ -0,0 +1,21 @@
+package com.mp.springidol.classes;
+
+public class Audience {
+	
+	public void takeSeats(){
+		System.out.println("Widzowie zajmuj¹ miejsca. ");
+	}
+	
+	public void turnOffCellPhones(){
+		System.out.println("Widzowie wy³¹czaj¹ telefony komórkowe. ");
+	}
+	
+	public void makeApplaud(){
+		System.out.println("Brawoooo!");
+	}
+	
+	public void demandRefund(){
+		System.out.println("S³aby wystêp..");
+	}
+
+}
